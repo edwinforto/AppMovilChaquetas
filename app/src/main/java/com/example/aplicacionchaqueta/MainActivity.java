d40cc4bd812a47fa48cac77a70d97a47f507585d
Edwin Forero <edwinjo023@gmail.com>
2021-11-30 21:40:21 -0500

    Finalizacion reto 2

--- app/src/main/java/com/example/aplicacionchaqueta/MainActivity.java
+++ app/src/main/java/com/example/aplicacionchaqueta/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.aplicacionchaqueta;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -16,10 +18,13 @@ public class MainActivity extends AppCompatActivity {
     /**
      * Declaracion de variables.
      */
-    private MenuItem productos;
-    private Button servicioss;
-    private Button sucursaless;
-
+    private Fragment mainScreen;
+    private Fragment prod;
+    private Fragment serv;
+    private Fragment suc;
+    private Fragment fav;
+    private Fragment shoppingCar;
+    private FragmentTransaction change;
 
     /**
      * inicialización de la pantalla Activity Main.
@@ -30,8 +35,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mainScreen = new FragmentMain();
+        prod = new Fragment_Productos();
+        serv = new Fragment_Servicios();
+        suc = new Fragment_Sucursales();
+        fav = new Fragment_Favoritos();
+        shoppingCar = new Fragment_ShoppingCar();
+
+        getSupportFragmentManager().beginTransaction().add(R.id.containerFragment,mainScreen).commit();
 
     }
+    //--------------------------MENU OPCIONES-------------------------------
 
     /**
      * creación del menu desplegable en la barra superior.
@@ -55,21 +69,37 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         if(id == R.id.menu1){
-            Intent secondScreen = new Intent(getApplicationContext(),MainActivity2.class);
-            startActivity(secondScreen);
+            change = getSupportFragmentManager().beginTransaction();
+            change.replace(R.id.containerFragment,prod).commit();
             Toast.makeText(this,"Ingreso a Productos", Toast.LENGTH_SHORT).show();
         }
         if(id == R.id.menu2){
-            Intent secondScreen = new Intent(getApplicationContext(),MainActivity3.class);
-            startActivity(secondScreen);
+            change = getSupportFragmentManager().beginTransaction();
+            change.replace(R.id.containerFragment,serv).commit();
             Toast.makeText(this,"Ingreso a Servicios ", Toast.LENGTH_SHORT).show();
         }
         if(id == R.id.menu3){
+            change = getSupportFragmentManager().beginTransaction();
+            change.replace(R.id.containerFragment,suc).commit();
             Toast.makeText(this,"Ingreso a Sucursales ", Toast.LENGTH_SHORT).show();
-            Intent secondScreen = new Intent(getApplicationContext(),MainActivity4.class);
-            startActivity(secondScreen);
+        }
+        if(id == R.id.iconB){
+            change = getSupportFragmentManager().beginTransaction();
+            change.replace(R.id.containerFragment,mainScreen).commit();
+            Toast.makeText(this,"Página Principal ", Toast.LENGTH_SHORT).show();
+        }
+        if(id == R.id.icon2_B){
+            change = getSupportFragmentManager().beginTransaction();
+            change.replace(R.id.containerFragment,fav).commit();
+            Toast.makeText(this,"Favoritos", Toast.LENGTH_SHORT).show();
+        }
+        if(id == R.id.icon3_B){
+            change = getSupportFragmentManager().beginTransaction();
+            change.replace(R.id.containerFragment,shoppingCar).commit();
+            Toast.makeText(this,"Carrito de compras", Toast.LENGTH_SHORT).show();
         }
         return super.onOptionsItemSelected(item);
     }
+    //-------------------------------------------------------------------------------------
 
 }
