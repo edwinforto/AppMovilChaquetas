b0dd6f48c95e24364cc00691b285177a7d29079c
Sebastián Suárez <sebse90@gmail.com>
2021-12-06 15:09:27 -0500

    Adición de pantalla Splash
    Se adiciona la pantalla Splash con barra de progreso.

---
+++ app/src/main/java/com/example/aplicacionchaqueta/Vista/Fragment_Sucursales.java
@@ -0,0 +1,33 @@
+package com.example.aplicacionchaqueta.Vista;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.aplicacionchaqueta.R;
+
+public class Fragment_Sucursales extends Fragment {
+
+    private View view;
+
+    public Fragment_Sucursales() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment__sucursales, container, false);
+        //--------------------------------------------------------------------------------
+
+
+        //--------------------------------------------------------------------------------
+
+        return view;
+    }
+}
